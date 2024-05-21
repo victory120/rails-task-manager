7bd015ff9bb4a968a2bdeefd3b5f4843f89ea880
victory agho <aghovictory120@gmail.com>
2024-05-21 16:27:12 -0700

    adding new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,21 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+  class TasksController < ApplicationController
+    def show
+      @task = Task.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to tasks_path, alert: "Task not found."
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
